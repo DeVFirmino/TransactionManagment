e405c3b689b5675b68999e365e0ca4ecd89b5676
devfirmino <daanspfc@gmail.com>
2025-11-25 02:54:59 +0100

    FEAT: Implement Transaction Input Validation
    Implement Fluent Validation for transaction processing (CreateTransactionDto).
    Ensures data integrity by enforcing:
    - Amount > 0
    - Custom rule for max 2 decimal places (financial precision).
    - Validates Transaction Type (IsInEnum).

---
+++ TransactionManagement/Migrations/20251125001211_IntialMigrationRelationshipBetweenGoalModelAndTransactionModelWithPkAndFK.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransactionManagment.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialMigrationRelationshipBetweenGoalModelAndTransactionModelWithPkAndFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Goals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FinancialGoalsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TargetQuantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Goals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FinancialGoalsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Goals_FinancialGoalsId",
+                        column: x => x.FinancialGoalsId,
+                        principalTable: "Goals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_FinancialGoalsId",
+                table: "Transactions",
+                column: "FinancialGoalsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Goals");
+        }
+    }
+}
