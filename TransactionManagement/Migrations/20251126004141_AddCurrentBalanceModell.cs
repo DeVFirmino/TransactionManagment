9ce5307712fd9c94c9d5b4ce98f8d0c17372de2b
devfirmino <daanspfc@gmail.com>
2025-11-27 02:17:52 +0100

    Adding Services and Repository

---
+++ TransactionManagement/Migrations/20251126004141_AddCurrentBalanceModell.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransactionManagment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrentBalanceModell : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
